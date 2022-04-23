de54b6d97ccdf224dc9023f8907d99b6c341336a
Daniel Ufeli <danielufeli@yahoo.com>
2022-04-23 14:41:31 +0100

    Add UI and Refactor Code

--- person.rb
+++ person.rb
@@ -1,12 +1,13 @@
 require_relative 'nameable'
+require_relative 'rental'
 
 class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age
 
-  def initialize(id, age, name = 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     super()
-    @id = id
+    @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
--- teacher.rb
+++ teacher.rb
@@ -1,13 +1,11 @@
-require_relative './person'
+require_relative 'person'
 
 class Teacher < Person
-  def initialize(id, age, specialization, name = 'Unknown', parent_permission: true)
-    super(id, age, name, parent_permission: parent_permission)
-    @specialization = specialization
-  end
+  attr_accessor :specialization
 
-  def play_hooky
-    "¯\(ツ)/¯"
+  def initialize(specialization, age, name: 'Unknown', parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
+    @specialization = specialization
   end
 
   def can_use_services?
